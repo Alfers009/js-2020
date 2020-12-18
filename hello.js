8e54173a0b9290d5ec11a69ae4183b7a218682fa
Alfers009 <74427063+Alfers009@users.noreply.github.com>
2020-12-18 22:45:11 +0200

    18.12

---
+++ hello.js
@@ -0,0 +1,14 @@
+var time =3000
+time=time + 1000
+var message = "Bine ai venit!"
+
+var isLate = true
+
+setTimeout(welcome, time)
+
+
+function welcome() {
+
+    alert(message)
+    alert("Welcome")
+}
