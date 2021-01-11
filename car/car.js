12c447466722d5ad29758eea14f7a437a04e183b
Alfers009 <74427063+Alfers009@users.noreply.github.com>
2021-01-11 20:55:55 +0200

    11.1

--- car/car.js
+++ car/car.js
@@ -1,32 +1,71 @@
 
 let dacia = {
-    make: "Dacia"
-    model: "1310"
-    color: "verde"
+    make: "Dacia",
+    model: 1310,
+    color: "verde",
     year: 1978,
     mileage: 525737,
     maxSpeed: 180,
-    paint: function(color) {
+    averageConsumption: 11.5,
+    batteryCapacity: 150,
+    batteryCharge: 150,
+    paint (color) {
         this.color = color
-        
+
+    },
+
+    move (distance) {
+        consumtion = this.averageConsumption * distance / 100
+        if (consumtion > this.batteryCharge) {
+            this.batteryCharge = 0
+            this.mileage += this.distanceToEmpty
+
+        } else {
+
+            this.batteryCharge -= consumtion
+            this.mileage += distance
+
+        }
+
+        this.batteryCharge -= consumtion
+        this.mileage += distance
+
+    },
+
+
+    recharge () {
+
+        this.batteryCharge = this.batteryCapacity
+
+    },
+
+    distanceToEmpty() {
+
+        return 100 * this.batteryCharge / this.averageConsumption
     }
+
+
 
 }
 
 let ferrari = {
-    make: "Ferrari"
-    model: "Dino"
-    color: "rosu"
+    make: "Ferrari",
+    model: "Dino",
+    color: "rosu",
     year: 1978,
     mileage: 525737,
     maxSpeed: 180,
-    paint: function(color) {
+    paint (color) {
         this.color = color
-        
+
     }
 
 }
-dacia.paint ("galben")
+dacia.paint("galben")
+dacia.move((100))
+console.log(Math.round(dacia.distanceToEmpty()))
+dacia.move((100))
+console.log(dacia.distanceToEmpty())
 console.log(dacia)
 console.log(ferrari)
 
---
+++ length/length.js
@@ -0,0 +1,9 @@
+document.getElementById("text")
+    .addEventListener("change", function () {
+        input = document.getElementById("text")
+        text = input.value
+        document.getElementById("length").innerText = text.length
+        
+    })
+
+    //tema ex2  3(java core script 4)
