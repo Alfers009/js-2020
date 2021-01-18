c06226cf34c928ae04ae3265fb10c7b3d665fb5b
Alfers009 <74427063+Alfers009@users.noreply.github.com>
2021-01-18 20:54:16 +0200

    18.1

--- array/array.js
+++ array/array.js
@@ -7,5 +7,19 @@ max = bubbles.pop ()
 fruits.sort()
 fruits.reverse()
 
+bubbles[0]
+fruits[2]
 console.log(max)
 console.log(fruits)
+
+for (let index = 0; index < fruits.length; index++) {
+    const fruit = fruits[index];
+    let listItem = document.createElement("li")
+    listItem.innerText = fruit
+    document.querySelector("ul").appendChild(listItem)
+    
+}
+
+for (const fruit of fruits) {
+        
+}
--- bubbles/bubbles.js
+++ bubbles/bubbles.js
@@ -9,8 +9,14 @@ function newBubble() {
     bubble.classList.add("bubble")
     let x =randomNumber(100)
     let delay = randomNumber(5000)
-    bubble.style.left = x + "vw"
-    bubble.style.animationDelay = delay + "ms"
+    let red = randomNumber(255)
+    let green  = randomNumber(255)
+    let blue = randomNumber(255)
+    let size = randomNumber(3) + 1
+    bubble.style.left = `${x}vw`
+    bubble.style.animationDelay = `${delay}ms`
+    bubble.style.borderColor = `rgb(${red}, ${green}, ${blue})`
+    bubble.style.height =  bubble.style.width = `${size}em`
     document.querySelector("body").appendChild(bubble)
 
 }
