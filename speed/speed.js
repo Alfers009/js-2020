1b02b27ecfef283f4616ff06bf09dadb94e1e2f2
Alfers009 <74427063+Alfers009@users.noreply.github.com>
2021-02-19 17:53:12 +0200

    19.2

--- speed/speed.js
+++ speed/speed.js
@@ -1,52 +1,65 @@
 let startButton = document.querySelector("input[type='button']")
 
-
 startButton.addEventListener("click", startGame)
 
 function startGame() {
-        startButton.classList.add("hidden")
-        let interval = 1000
-        setInterval(createNewLetter, interval);
-        createNewLetter()
+    // ascunde butonul
+    startButton.classList.add("hidden")
+    // intervalul este de o secunda (1000ms)
+    let interval = 1000
+    // cream o noua litera dupa fiecare secunda
+    setInterval(createNewLetter, interval);
 }
 
-function randomNumber(max) {
-    Math.floor(Math.random() * max + 1) + min
+// genereaza un numar aleatoriu intre min si max (inclusiv)
+function randomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min
 }
 
+// genereaza o litera aleatorie mare intre A si Z
 function randomLetter() {
+    // generam un cod ascii aleatoriu intre 65 ('A') si 90 ('Z')
     let codeOfA = "A".charCodeAt(0)
     let codeOfZ = "Z".charCodeAt(0)
     let randomCode = randomNumber(codeOfA, codeOfZ)
+    // convertim codul ascii in caracterul asociat lui
     return String.fromCharCode(randomCode)
-
 }
 
+// genereaza o culoare aleatorie
 function randomColor() {
-    let red = randomNumber(0,255)
-    let green = randomNumber(0,255)
-    let blue = randomNumber(0,255)
+    let red = randomNumber(0, 255)
+    let green = randomNumber(0, 255)
+    let blue = randomNumber(0, 255)
     return `rgb(${red},${green},${blue})`
 }
 
+// genereaza o pozitie aleatorie intre 0% si 90%
 function randomPosition() {
-    let random = randomNumber(0,90)
+    let position = randomNumber(0, 90)
     return `${position}%`
 }
 
+// creaza un nou element div cu o litera aleatorie si
+// adauga elementul in html
 function createNewLetter() {
     let letter = randomLetter()
     let color = randomColor()
     let top = randomPosition()
     let right = randomPosition()
-    let div = dcoument.createElement ("div")
-    //
+    // cream un nou element div (intial este gol si nu are stil)
+    let div = document.createElement("div")
+    // adagam clasa "letter" elementului nostru
     div.classList.add("letter")
-    div.innertext = letter
-    div.style.backroundColor = color
-    div.stle.top = top
+    // setam textul din interiorul div-ului sa fie litera aleatorie
+    div.innerText = letter
+    // setam culoarea css
+    div.style.backgroundColor = color
+    // setam top si right in css
+    div.style.top = top
     div.style.right = right
+    // setam o clasa cu valoarea literei aleatorii
     div.classList.add(letter)
+    // adaugam elementul div in body
     document.querySelector("body").appendChild(div)
 }
-
