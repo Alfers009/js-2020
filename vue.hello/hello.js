eeff74078f360a0501fa7ec66ca3f044f1da596a
Alfers009 <74427063+Alfers009@users.noreply.github.com>
2021-04-18 21:40:10 +0300

    18.4

--- vue.hello/hello.js
+++ vue.hello/hello.js
@@ -1,3 +1,34 @@
 let app = new Vue({
-    el:'#app'
+    el:'#app',
+    data:{
+        counter:0
+    },
+    methods:{
+        increment() {
+            this.counter++
+        }
+    }
+    computed: {
+        time(){
+            let time = new Date()
+            return time.toTimeString()
+
+        },
+        message(){
+            let hour = new Date().getHours()
+            if (hour < 12 && hour >1) {
+                return "Buna Dimineata"
+            
+            } else if (hour >=12 && hour <18) {
+                return "Buna ziua!"
+            
+            } else if (hour >=18 && hour <=23) {
+                return "Buna seara!"
+            
+            } else {
+                return"Somn usor"
+            }
+        }
+    }
 })
+
